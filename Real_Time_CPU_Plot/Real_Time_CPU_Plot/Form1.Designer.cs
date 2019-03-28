eb04d77d5a0e63609a3aa76e8d914ff13dc7f283
ackyhn <44120221+ackyhn@users.noreply.github.com>
2019-03-28 14:55:18 -0400

    Add files via upload

---
+++ Real_Time_CPU_Plot/Real_Time_CPU_Plot/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Real_Time_CPU_Plot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.CPU_chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CPU_chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CPU_chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.CPU_chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.CPU_chart1.Legends.Add(legend2);
+            this.CPU_chart1.Location = new System.Drawing.Point(121, 34);
+            this.CPU_chart1.Name = "CPU_chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.CPU_chart1.Series.Add(series2);
+            this.CPU_chart1.Size = new System.Drawing.Size(799, 231);
+            this.CPU_chart1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(976, 101);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(123, 31);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(976, 187);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(123, 31);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(976, 276);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(123, 31);
+            this.btnResume.TabIndex = 3;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(976, 358);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(123, 31);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 486);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.CPU_chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CPU_chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart CPU_chart1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Real_Time_CPU_Plot/Real_Time_CPU_Plot/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Diagnostics;
+
+namespace Real_Time_CPU_Plot
+{
+    public partial class Form1 : Form
+    {
+        private Thread cpuThread;
+        private double[] cpuArrays = new double[30];
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnStop.Enabled = false;
+            btnResume.Enabled = false;
+        }
+
+        private void getPerformanceCounter()
+        {
+            var cpuPerCounter = new PerformanceCounter("Processor information", "% Processor Time", "_Total");
+
+            while (true)
+            {
+                cpuArrays[cpuArrays.Length - 1] = Math.Round(cpuPerCounter.NextValue(), 0);
+                Array.Copy(cpuArrays, 1, cpuArrays, 0, cpuArrays.Length - 1);
+
+                if (CPU_chart1.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { updateCpuChart(); });
+                }
+                else
+                {
+                    //...........
+                }
+                Thread.Sleep(100);
+            }
+            
+        }
+        private void updateCpuChart()
+        {
+            CPU_chart1.Series[0].Points.Clear();
+            for (int i = 0; i < cpuArrays.Length - 1; ++i)
+            {
+                CPU_chart1.Series[0].Points.AddY(cpuArrays[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cpuThread = new Thread(new ThreadStart(this.getPerformanceCounter));
+            cpuThread.IsBackground = true;
+            cpuThread.Start();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {           
+            if (btnStart.Enabled == false)
+            {
+                cpuThread.Abort();
+                
+                btnStop.Enabled = false;
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    
+}
